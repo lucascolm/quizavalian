09b84f3ef13458a1d5b0f1df82a864d743476852
lucascolm <lucaspaganelli20@gmail.com>
2023-02-16 22:45:51 -0300

    first commit

---
+++ src/preguntas.js
@@ -0,0 +1,46 @@
+const preguntas=[
+  {
+    titulo:"¿Cuántas bandas tocan en COSQUIN ROCK 2023?",
+    opciones:[
+      {respuesta:"95",isCorrect:false},
+      {respuesta:"107",isCorrect:true},
+      {respuesta:"100",isCorrect:false},
+    ],
+  },
+  {
+    titulo:"¿Quíen cierra el escenario Norte el día 1?",
+    opciones:[
+      {respuesta:"Juanse",isCorrect:true},
+      {respuesta:"No te va gustar",isCorrect:false},
+      {respuesta:"Skay",isCorrect:false},
+    ],
+  },
+  {
+    titulo:"¿Cuántas hectáreas tiene el predio del festival? ",
+    opciones:[
+      {respuesta:"9",isCorrect:true},
+      {respuesta:"3",isCorrect:false},
+      {respuesta:"15",isCorrect:false},
+    ],
+  },
+  {
+    titulo:"¿Cómo se llama el DJ internacional que toca en el escenario de montaña el día 2?",
+    opciones:[
+      {respuesta:"Paul Oakenfold",isCorrect:false},
+      {respuesta:"Calvin Harris",isCorrect:false},
+      {respuesta:"TIËSTO",isCorrect:true},
+      
+    ],
+  },
+  {
+    titulo:"¿Cuántos escenarios hay en COSQUIN ROCK 2023?",
+    opciones:[
+      {respuesta:"6",isCorrect:true},
+      {respuesta:"4",isCorrect:false},
+      {respuesta:"5",isCorrect:false},
+    ],
+  },
+]
+
+
+export default preguntas
